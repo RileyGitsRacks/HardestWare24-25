5c9c8cf208ff4a6e22120faefe97d7381e474ddd
RileyGitsRacks <bretthar@wlhs.wlwv.k12.or.us>
2023-12-06 10:32:43 -0800

    weird motor fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMecanum.java
@@ -113,7 +113,7 @@ public class HardwareMecanum {
         // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
         // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
         // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
-        frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        frontLeftDrive.setDirection(DcMotor.Direction.FORWARD);
         frontRightDrive.setDirection(DcMotor.Direction.FORWARD);
         backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
         backRightDrive.setDirection(DcMotor.Direction.FORWARD);
